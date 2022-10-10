475dc64626ec3e5e5ea870e023367f6ff488ab2a
Robert Sheffield <sheffira2@gmail.com>
2022-10-10 16:38:05 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Router from 'svelte-spa-router';
+
+	import Home from './pages/Home.svelte';
+	import Login from './pages/Login.svelte';
+	import Cart from './pages/Cart.svelte';
+</script>
+
+<main>
+	<nav>
+		<a href="/#/">Home</a>
+		<a href="/#/login">Login</a>
+		<a href="/#/cart">Cart</a>
+	</nav>
+	<button on:click|preventDefault={() => {document.body.classList.toggle('dark-theme')}}>Switch Mode</button>
+	<Router routes={{
+		'/': Home,
+		'/login': Login,
+		'/cart': Cart
+	}} />
+</main>
+
+<style>
+	
+</style>
