ddbfde090fa18cd836b0806dddc284b00de5fc30
Robert Sheffield <sheffira2@gmail.com>
2023-01-30 20:53:01 -0800

    Forgot part of the server url

--- src/hosts.js
+++ src/hosts.js
@@ -2,6 +2,6 @@ const FRONTDEV = false;
 const SERVERDEV = false;
 
 export const frontendHost = FRONTDEV ? "http://localhost:8080/#/" : "https://gorgeous-sawine-faaa7b.netlify.app/#/";
-export const serverHost = SERVERDEV ? "http://localhost:8000/api/" : 'https://web-production-8fd9.up.railway.app/'
+export const serverHost = SERVERDEV ? "http://localhost:8000/api/" : 'https://web-production-8fd9.up.railway.app/api/'
 
 
